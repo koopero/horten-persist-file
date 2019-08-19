c0bf66bb5183e7397d86c8c4ee873f4b92ee79c6
Samm Cooper <samm@hfour.ca>
2019-08-19 12:45:12 -0700

    misc, ring loop first attempt

--- src/HortenPersistFile.js
+++ src/HortenPersistFile.js
@@ -1,7 +1,7 @@
 const H = require('horten')
 const _ = require('lodash')
 const fs = require('fs-extra')
-const path = require('path')
+const pathlib = require('path')
 const yaml = require('js-yaml')
 const moment = require('moment')
 
@@ -12,33 +12,37 @@ class HortenPersistFile extends H.Cursor {
     _.defaults( options, {
       delay: 100,
       delayMax: 1000,
-      listening: true
+      listening: true,
+      file: '',
+      format: '',
+      count: 0,
     })
     super( options )
     this.on('value', this.onValue.bind( this ) )
+    this.ringIndex = 0
   }
 
   configure( options ) {
     super.configure( options )
-    this.file = path.resolve( options.file || DEFAULT_FILE )
+    this.file = pathlib.resolve( options.file || DEFAULT_FILE )
     this.format = options.format == 'yaml' ? 'yaml'
-      : ['.yaml','.yml'].indexOf( path.extname( this.file ) ) != -1 ? 'yaml'
+      : ['.yaml','.yml'].indexOf( pathlib.extname( this.file ) ) != -1 ? 'yaml'
         : 'json'
+
+    this.count = parseInt( options.count ) || 0
 
     if ( options.open )
       this.open()
 
-
-    // console.log('configure', options )
   }
 
-  onValue( value ) {
+  onValue() {
     if ( !this.writing ) {
       this.write()
     }
   }
 
-  open() {
+  async open() {
     const self = this
     return self.read()
       .catch( function () {
@@ -46,11 +50,14 @@ class HortenPersistFile extends H.Cursor {
       } )
   }
 
-  read( src ) {
+  async read( src ) {
     const self = this
+    if ( !src ) {
+
+    }
     src = src || this.file
 
-    return fs.readFileAsync( src, 'utf8')
+    return fs.readFile( src, 'utf8')
       .then( this.format == 'yaml' ? yaml.load : JSON.parse )
       .then( function ( data ) {
         self.patch( data )
@@ -58,35 +65,59 @@ class HortenPersistFile extends H.Cursor {
       })
   }
 
+  fileToWrite() {
+    if ( this.count <= 1 )
+      return this.file 
+
+    let file = addExtensionPrefix( this.file, '.' + this.ringIndex )
+    this.ringIndex = ( this.ringIndex + 1 ) % this.count
+    return file
+  }
+
+
   header() {
     return '# written by horten-persist-file at '+moment().format()+'\n'
   }
 
-  write( dest ) {
-    const self = this
+  async write( dest ) {
     const data = this.value
 
-    dest = dest || this.file
+    dest = dest || this.fileToWrite()
 
     var str
 
     switch ( this.format ) {
       case 'yaml':
         str = this.header() + yaml.dump( data )
-      break
+        break
 
       default:
         str = JSON.stringify( data )
-      break
+        break
     }
 
-    self.writing = true
-    return fs.outputFileAsync( dest, str )
-    .then( function () {
-      self.writing = false
-      self.emit('write', dest )
-    } )
+    this.writing = true
+    await fs.outputFile( dest, str )
+    this.writing = false
+    this.emit('write', dest )
   }
 }
 
 module.exports = HortenPersistFile
+
+
+//
+// Util functions below
+//
+
+function addExtensionPrefix( file, prefix ) {
+  let parsed = pathlib.parse( file )
+  let result = pathlib.join( parsed.dir, parsed.name + prefix + parsed.ext )
+  console.log( result, parsed )
+  // process.exit()
+  return result
+}
+
+async function findNewestFile( file ) {
+  const globlib = require('glob-promise')
+}
--- test/HortenPersistFile-test.js
+++ test/HortenPersistFile-test.js
@@ -1,21 +1,62 @@
 const test = require('./_test')
-
+const pathlib = require('path')
+const resolveTmp = pathlib.resolve.bind( null, __dirname, 'tmp' )
 const fs = require('fs-extra')
-const path = require('path')
-
-const HortenPersistFile = require('../index')
-
-const file = new HortenPersistFile( {
-  delayMax: 100,
-  file: path.resolve( __dirname, 'tmp/', 'persist.json' )
-} )
+const assert = require('chai').assert
 
 
-const root = H.root
+describe('horten-persist-file', () => {
 
-mutate()
+  beforeEach( async () => {
+    fs.remove( pathlib.resolve( __dirname, 'tmp/' ) )
+  })
 
-function mutate() {
-  root.patch( test.number(), test.path() )
-  setTimeout( mutate, 50 )
-}
+  it('will write a file', ( done ) => {
+    const H = require('horten')
+    const HortenPersistFile = require('../index')
+    const root = new H.Mutant()
+    const file = resolveTmp( 'persist.json' )
+    const data = test.number()
+    const path = test.path()
+    const persist = new HortenPersistFile( { root, file } )
+
+    persist.on('write', ( wrotefile ) => {
+      assert.equal( file, wrotefile )
+      let wroteData = fs.readJSONSync( file )
+      assert.deepEqual( wroteData, root.get() )
+      done()
+    })
+
+    root.patch( data, path )
+  })
+
+  it('will write multiple files in ring', async () => {
+    const H = require('horten')
+    const HortenPersistFile = require('../index')
+    const root = new H.Mutant()
+    const file = resolveTmp( 'ring.json' )
+    const glob = resolveTmp( 'ring.*.json' )
+    
+    // Ensure empty before starting
+    assert.equal( countFiles(), 0 )
+
+    const count = 3
+    const persist = new HortenPersistFile( { root, file, count } )
+
+    for ( let i = 0; i < count * 2; i ++ ) {
+      const data = test.number()
+      const path = test.path()
+      root.patch( data, path )
+      await new Promise( ( cb ) => setTimeout( cb, 200 ) )
+    }
+
+    assert.equal( countFiles(), count )
+
+    function countFiles() {
+      const globlib = require('glob')
+      let files = globlib.sync( glob )
+      return files.length
+    }
+  } )
+})
+
